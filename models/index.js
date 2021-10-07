f29fc54b153f8b0ad1851b68ee910f543b485298
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-10-07 16:06:46 -0700

    updated model for index.js, built associations

--- models/index.js
+++ models/index.js
@@ -1,13 +1,31 @@
 const User = require('./User');
-const Project = require('./Project');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-User.hasMany(Project, {
+// Building associations
+User.hasMany(Post, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Post.belongsTo(User, {
   foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
 });
 
 module.exports = { User, Project };

8a5047c92d5b8d2b223743d4e5b006eb4f933618
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-10-09 18:00:34 -0700

    added get routes

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment} = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            // use the ID from the session
+            user_id: req.session.user_id   
+        },
+        attributes: [
+            'id',
+            'title',
+            'created_at',
+            'post_content'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post-id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username', 'github']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username', 'github']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        // serialize data before passing to template
+    })
+})

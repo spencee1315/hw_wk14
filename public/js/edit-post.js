4c0ec96c503baa3b332c1a9d680cc514f18d7de6
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-10-25 21:15:00 -0700

    completed edit-post.js

---
+++ public/js/edit-post.js
@@ -0,0 +1,32 @@
+const { doc } = require("prettier");
+
+// creating function for editing post content on dashboard
+async function editFormHandler(event) {
+    event.preventDefault();
+
+    // adding parameters of post, to select the correct document to edit
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_content = document.querySelector('input[name="post-content"]').value;
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title,
+            post_content
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard/');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);

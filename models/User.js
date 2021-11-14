7b9e1469641ea4febb2146c704094be69741d3ce
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-11-14 15:01:04 -0800

    debugging

--- models/User.js
+++ models/User.js
@@ -25,6 +25,10 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    twitter: {
+      type: DataTypes.STRING,
+      allowNull: true
+  },
     github: {
       type: DataTypes.STRING,
       allowNull: true

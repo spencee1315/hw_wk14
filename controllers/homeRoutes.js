390c64013e270e16341a1153f5722472743cf9a8
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-11-26 11:10:33 -0800

    updated get, post.findAll route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -52,24 +52,42 @@ router.get('/Post/:id', async (req, res) => {
   }
 });
 
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Post }],
-    });
+router.get('/', (req, res) => {
+  console.log(req.session);
 
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true
+  Post.findAll({
+    attributes: [
+      'id',
+      'title',
+      'created_at',
+      'post_content'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username', 'github']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username', 'github']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true}));
+      res.render('homepage', {
+        posts,
+        logged_in: req.session.logged_in
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
 router.get('/login', (req, res) => {
